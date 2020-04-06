e598aaa4de6ec0c288556b7714afb470747f3a0a
booknap <napasai45@gmail.com>
2020-04-06 22:47:50 +0700

    first

---
+++ src/main/java/CoursesController.java
@@ -0,0 +1,8 @@
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class CoursesController {
+    public void courses() {
+        System.out.println("Welcome to Eureka");
+    }
+}
---
+++ src/main/java/WebAppApplication.java
@@ -0,0 +1,7 @@
+import org.springframework.boot.SpringApplication;
+
+public class WebAppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebAppApplication.class, args);
+    }
+}
